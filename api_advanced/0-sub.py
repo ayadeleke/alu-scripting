b6e8b584a3ed21879b141e32f447ae3de9cf590f
ayadeleke <a.adeleke@alustudent.com>
2023-07-15 01:05:51 +0200

    initial commit

--- api_advanced/0-sub.py
+++ api_advanced/0-sub.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-""" How many subs"""
+"""
+How many subs
+"""
 
 import requests
 
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json"
-        .format(subreddit)
+    .format(subreddit)
     headers = {'User-Agent': 'My-User-Agent'} 
     
-    Response = reqeust
+    Response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+            return 0
+    else:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
