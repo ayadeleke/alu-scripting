7dcd6af0dac981b7132a74bf5ec31d62c31c0b9d
ayadeleke <https://github.com/ayadeleke>
2023-07-14 16:44:22 -0700

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -3,16 +3,15 @@
 
 import requests
 
-
 def top_ten(subreddit):
     """Top ten subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'My- User-Agent'}
+    headers = {'User-Agent': 'My-User-Agent'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-             posts = data.get('data').get('children')
+    if response.status_code == 200:
+        data = response.json()
+        posts = data.get('data').get('children')
         [print(post.get('data').get('title')) for post in posts]
-        else:
-            print(None)
+    else:
+        print(None)
