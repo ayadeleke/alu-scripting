8ab1afb07cae516c6e222c744f362f83783d0264
ayadeleke <https://github.com/ayadeleke>
2023-07-14 16:22:29 -0700

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -3,16 +3,16 @@
 
 import requests
 
-def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json"
-    .format(subreddit)
-    headers = {'User-Agent': 'My-User-Agent'} 
-    
-    Response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code != 200:
+def number_of_subscribers(subreddit):
+    """Reddit subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json" \
+        .format(subreddit)
+    headers = headers={"User-Agent": "My-User-Agent"}
+
+
+     response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
             return 0
-    else:
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
+        else:
+            return response.json().get("data").get("subscribers")
