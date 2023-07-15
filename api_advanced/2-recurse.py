7ea80b616c0a0614437a1605a9b440d056d94550
ayadeleke <https://github.com/ayadeleke>
2023-07-14 17:19:45 -0700

    2-recurse.py

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,24 +1,26 @@
-#!/usr/bin/python3
-"""Contains recurse function"""
 import requests
 
-
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """Recursive function for the hot list retrieval"""
+def recurse(subreddit, hot_list=None, after=None, count=0):
+    """Recursive function for retrieving the hot list of a subreddit"""
+    if hot_list is None:
+        hot_list = []
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'My-User-Agent'}
-    params = {"count": count, "after": after} 
+    params = {"count": count, "after": after}
 
     response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    if response.status_code == 400:
+    if response.status_code != 200:
         return None
-    else:
-        data = response.json().get("data")
-        after = data.get("after")
-        count += data.get("dist")
-        for c in data.get("children"):
-            hot_list.append(c.get("data").get("title"))
 
-        if after is not None:
-            return recurse(subreddit, hot_list, after, count)
+    data = response.json().get("data")
+    after = data.get("after")
+    count += data.get("dist")
+
+    for post in data.get("children"):
+        title = post.get("data").get("title")
+        hot_list.append(title)
+
+    if after:
+        return recurse(subreddit, hot_list, after, count)
+    else:
         return hot_list
---
+++ api_advanced/3-count.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""3-count.py"""
+
+import json
+import requests
+
+
+def count_words(subreddit, word_list, after="", count=[]):
