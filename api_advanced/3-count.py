792618cfb6b5285eaee522455e1016be83c595c3
ayadeleke <https://github.com/ayadeleke>
2023-07-15 04:22:39 -0700

    3-count.py

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-"""recursive function that queries the Reddit API
-"""
+"""ecursive function that queries the Reddit API, parse hot articles"""
 import json
 import requests
 
-def count_words(subreddit, word_list, after="", count=None):
+
+def count_words(subreddit, word_list, after=None, counts=None):
     """Recursive function to count words"""
-    if count is None:
-        count = [0] * len(word_list)
+    if counts is None:
+        counts = {word.lower(): 0 for word in word_list}
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "My-User-Agent"}
     params = {"after": after}
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False
+    )
     if response.status_code != 200:
         return None
 
@@ -22,23 +27,25 @@ def count_words(subreddit, word_list, after="", count=None):
     posts = data.get("children")
 
     for post in posts:
-        title = post.get("data").get("title")
+        title = post.get("data").get("title").lower()
         words = title.split()
 
         for word in word_list:
-            for w in words:
-                if w.lower() == word.lower():
-                    count[word_list.index(word)] += 1
+            if word.lower() in words:
+                counts[word.lower()] += 1
 
     if after:
-        return count_words(subreddit, word_list, after, count)
+        return count_words(subreddit, word_list, after, counts)
     else:
-        sorted_counts = sorted(zip(word_list, count), key=lambda x: (-x[1], x[0].lower()))
+        sorted_counts = sorted(
+            counts.items(),
+            key=lambda x: (-x[1], x[0])
+        )
         for word, count in sorted_counts:
-            print("{}: {}".format(word.lower(), count))
+            print("{}: {}".format(word, count))
 
     # Base case: Return counts
-    return count
+    return counts
 
 def recurse(subreddit, word_list):
     """Wrapper function for the recursive count_words function"""
