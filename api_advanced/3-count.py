1acc8c95e2a2f951cab607fb1dee5039ff181827
ayadeleke <https://github.com/ayadeleke>
2023-07-15 05:44:09 -0700

    3-count.py

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""A recursive function that queries the Reddit API,
+""" a recursive function that queries the Reddit API,
 parses the title of all hot articles, and prints a sorted
 count of given keywords (case-insensitive, delimited by spaces.
 Javascript should count as javascript, but java should not)."""
-
 import json
 import requests
 
@@ -11,7 +10,7 @@ import requests
 def count_words(subreddit, word_list, after='', word_dict={}):
     """ If no post or the subreddit is invalid, it prints nothing.
     """
-	
+
     if not word_dict:
         for word in word_list:
             if word.lower() not in word_dict:
@@ -28,7 +27,7 @@ def count_words(subreddit, word_list, after='', word_dict={}):
     header = {'user-agent': 'my-user-agent'}
     params = {'limit': 100, 'after': after}
     response = requests.get(url, headers=header, params=params,
-                            allow_redirects=False
+                            allow_redirects=False)
 
     if response.status_code != 200:
         return None
@@ -41,7 +40,7 @@ def count_words(subreddit, word_list, after='', word_dict={}):
             lower = [word.lower() for word in title.split(' ')]
 
             for word in word_dict.keys():
-                word_dict[word] += lower.count(word.lower())
+                word_dict[word] += lower.count(word)
 
     except Exception:
         return None
